7c89801072e375ccdf577b070253e3c6dbc4d421
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-30 11:28:07 +0900

    project init

---
+++ src/main/java/com/example/spartalock/SpartaLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.spartalock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spartalock/SpartaLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartalock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
